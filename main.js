3c9366fb6b817e18fba1ff2064933e8681b724ac
andreas lööv <Andreas.loov@hotmail.com>
2022-07-05 15:35:33 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,359 @@
+const redditNewsFeed = [
+    {
+        content: `A teacher at my school was sick of remembering all the different 
+        bell schedules, so I created an app to make things easier. 
+        The whole school loves it.`,
+        user: `u/scheduled_nightmare`,
+        score: `259`,
+    },
+    {
+        content: `Autoslap - a node CLI tool that automatically installs 
+        and configures ESLint, Prettier, Husky and lint-staged`,
+        user: `u/paolostyle`,
+        score: `13`,
+    },
+    {
+        content: `Pass by reference !== pass by value`,
+        user: `u/mburakerman`,
+        score: `2853`,
+    },
+    {
+        content: `After learning JS more than a year ago and 
+        falling in love with React Native, this is my first project, Tour, 
+        a drag-drop trip planner.`,
+        user: `u/VanaticalDesign`,
+        score: `500`,
+    },
+    {
+        content: `Nobody talks about the real reason to use Tabs over Spaces`,
+        user: `u/ChaseMoskal`,
+        score: `318`,
+    },
+    {
+        content: `Oracle Owns "Javascript", so Apple is taking down my app!`,
+        user: `u/imacpro1`,
+        score: `899`,
+    },
+    {
+        content: `Moment.js Throws in the Towel: "It is not dead, but it is indeed done."`,
+        user: `u/ILikeChangingMyMind`,
+        score: `756`,
+    },
+    {
+        content: `Github private repositories are free now`,
+        user: `u/Kumagor0`,
+        score: `978`,
+    },
+    {
+        content: `Someone posted a video showing off their pseudo desktop UI on r/webdev and 
+        I tried to make my version of it as a self challenge.`,
+        user: `u/_-__-_-__-__-`,
+        score: `5`,
+    },
+    {
+        content: `Microsoft won't support IE8, IE9 and IE10 anymore after January 12, 2016`,
+        user: `u/kasperpeulen`,
+        score: `967`,
+    },
+    {
+        content: `Water dynamics in my evolution simulator built in TypeScript / WebGL2`,
+        user: `u/FredrikNoren`,
+        score: `106`,
+    },
+]
+
+
+const javascriptHistory = [
+    {
+        content: 'Object.keys() - JavaScript | MDN',
+        timeAgo: '1 hour ago'
+    },
+    {
+        content: 'length of object javascript - Search on google',
+        timeAgo: '1 hour ago'
+    },
+    {
+        content: 'filter array javascript - Search on google',
+        timeAgo: '3 hours ago'
+    },
+    {
+        content: 'Self-Driving Car with JavaScript Course - Neural Networks and Machine Learning - Youtube',
+        timeAgo: '4 hours ago'
+    },
+    {
+        content: 'Create multiple div using javascript - Search on Google',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'JavaScript | MDN',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'Useful JavaScript links',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'Online interactive JavaScript (JS) Cheat Sheet',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'How to handle monetary values in JavaScript | frontstuff',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'Webstorm: The smartest JavaScript IDE, by JetBrains',
+        timeAgo: '1 day ago'
+    },
+    {
+        content: 'Strict inequality (!==) - JavaScript | MDN',
+        timeAgo: '2 days ago'
+    },
+    {
+        content: 'r/javascript - Search on Google',
+        timeAgo: '2 days ago'
+    },
+    {
+        content: 'multiline string javascript - Search on Google',
+        timeAgo: '2 days ago'
+    },
+    {
+        content: 'Array.prototype.map() - JavaScript | MDN',
+        timeAgo: '2 days ago'
+    },
+    {
+        content: 'parameter onclick javascript - Search on Google',
+        timeAgo: '4 days ago'
+    },
+    {
+        content: 'Live edit in HTML, CSS and JavaScript | Jetbrains Rider',
+        timeAgo: '4 days ago'
+    },
+    {
+        content: 'Advanced TypeScript: Mapped types and more : javascript',
+        timeAgo: '4 days ago'
+    },
+    {
+        content: 'create element javascript - Search on Google',
+        timeAgo: '5 days ago'
+    },
+    {
+        content: 'Pass parameter to JavaScript function onclick in HTML | Example code',
+        timeAgo: '5 days ago'
+    },
+    {
+        content: 'usestate react javascript - Search on Google',
+        timeAgo: '5 days ago'
+    },
+    {
+        content: 'useffect react javascript - Search on Google',
+        timeAgo: '5 days ago'
+    },
+]
+
+const githubIssues = [
+    {
+        repo: 'Repo: Googlechrome/puppeteer',
+        issueNumber: 'Issue #1341',
+        content: `How to get the inner text elements from the 
+        return values of page.$$`,
+    },
+    {
+        repo: 'Repo: JamesNK/NewtonSoft.Json',
+        issueNumber: 'Issue #1589',
+        content: `Newtonsonft.Json.Linq.JObject.Add methods
+        do not document that they clone their inputs`,
+    },
+    {
+        repo: 'Repo: JamesNK/NewtonSoft.Json',
+        issueNumber: 'Issue #1685',
+        content: `Why does the documentation not include the
+        Exceptions thay may be thrown?`,
+    },
+    {
+        repo: 'Repo: JamesNK/NewtonSoft.Json',
+        issueNumber: 'Issue #2457',
+        content: `ALEPH-2018004 - DOS vulnerability`,
+    },
+    {
+        repo: 'Repo: dotnet/runtime',
+        issueNumber: 'Issue #69320',
+        content: `Can the BlockingCollection<T>.GetConsumingEnumerable
+        iterator be optimized?`,
+    },
+    {
+        repo: 'Repo: dotnet/runtime',
+        issueNumber: 'Issue #70785',
+        content: `JsonNode? control = JsonNode.Parse(json) => 
+        System.ArgumentOutOfRangeException`,
+    },
+    {
+        repo: 'Repo: dotnet/msbuild',
+        issueNumber: 'Issue #7651',
+        content: `Unnecessary references to old packages in .NET 6 NuGet`,
+    },
+    {
+        repo: 'Repo: OpenLiveWriter/OpenLiveWriter',
+        issueNumber: 'Issue #378',
+        content: `Can't open blogger "page" for editing`,
+    },
+    {
+        repo: 'Repo: OpenLiveWriter/OpenLiveWriter',
+        issueNumber: 'Issue #751',
+        content: `Error When Authorizing Blogger Account On New Machine -
+        Can't Connect To Your Blog Service Backend Error 500`,
+    },
+    {
+        repo: 'Repo: OpenLiveWriter/OpenLiveWriter',
+        issueNumber: 'Issue #474',
+        content: `Can't publish or add blog due to https2https redirect`,
+    },
+    {
+        repo: 'Repo: dotnet/aspnetcore',
+        issueNumber: 'Issue #42321',
+        content: `The method 'AddPolicyRegistry' is called multiple times,
+        and only the last one takes effect`,
+    },
+    {
+        repo: 'Repo: facebook/react',
+        issueNumber: 'Issue #18178',
+        content: `Bug: too hard to fix "Cannot Update a component from 
+        inside the function body of a different component`,
+    },
+    {
+        repo: 'Repo: mui/material-ui',
+        issueNumber: 'Issue #4532',
+        content: `All buttons requires double click to trigger click event`,
+    },
+    {
+        repo: 'Repo: redux-form/redux-form',
+        issueNumber: 'Issue #860',
+        content: `Leaving a form takes two clicks`,
+    },
+]
+
+// For social media icons
+const handleClick = (url) => {
+    window.open(url);
+}
+
+// Digital Clock.
+const showTime = () => {
+    let date = new Date();
+
+    let year = date.getFullYear();
+
+    let month = date.toLocaleString('en-us', { month: 'long' });
+
+    let day = date.getDate();
+
+    let hour = date.getHours();
+
+    let minute = date.getMinutes();
+
+    // AM or PM. AM is default.
+    let period = "AM";
+
+    if (hour == 0) {
+        hour = 12;
+    }
+
+    if (hour > 12) {
+        hour = hour - 12;
+        period = "PM";
+    }
+
+    hour = (hour < 10) ? "" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+
+    let time = hour + ":" + minute;
+
+    document.getElementById('time').innerText = time;
+    document.getElementById('period').innerText = period;
+    document.getElementById('date').innerText = `${month} ${day}, ${year}`;
+
+    setTimeout(showTime, 1000);
+}
+
+// Create divs with mock data when website first loads.
+const onLoad = () => {
+
+    showTime();
+
+    // r/javascript scrollable newsfeed.
+    redditNewsFeed.forEach(post => {
+        let content = document.createElement('p');
+        content.className = 'content';
+        content.textContent = post.content;
+
+        let postedBy = document.createElement('p');
+        postedBy.className = 'postedBy';
+        postedBy.textContent = 'Posted by: ';
+        let user = document.createElement('span');
+        user.className = 'user'
+        user.textContent = post.user;
+        postedBy.append(user);
+
+        let score = document.createElement('p');
+        score.className = 'redditScore';
+        score.textContent = `Reddit Score: ${post.score}`;
+
+        let linkToComments = document.createElement('p');
+        linkToComments.className = 'linkToComments';
+        linkToComments.textContent = 'Link to Comments';
+
+
+        let newDiv = document.createElement('div');
+        newDiv.className = 'post'
+        newDiv.append(content);
+        newDiv.append(postedBy);
+        newDiv.append(score);
+        newDiv.append(linkToComments);
+
+        let redditPosts = document.getElementById('redditPosts')
+        redditPosts.append(newDiv);
+    });
+
+    // JavaScript from users own history.
+    javascriptHistory.forEach(post => {
+        let postsContent = document.createElement('p');
+        postsContent.className = 'postsContent'
+
+        let postBody = document.createElement('span');
+        postBody.className = 'postBody';
+        postBody.textContent = post.content;
+        postsContent.append(postBody);
+
+        let postTime = document.createElement('span');
+        postTime.className = 'postTime';
+        postTime.textContent = post.timeAgo;
+        postsContent.append(postTime);
+
+        let historyContent = document.getElementById('historyContent');
+        historyContent.append(postsContent)
+
+    });
+
+    // Scrollable github issues.
+    githubIssues.forEach(issue => {
+        let repo = document.createElement('p');
+        repo.className = 'repo';
+        repo.innerText = issue.repo;
+
+        let issueNumber = document.createElement('p');
+        issueNumber.className = 'issueNumber';
+        issueNumber.innerText = issue.issueNumber;
+
+        let content = document.createElement('p');
+        content.className = 'content';
+        content.innerText = issue.content;
+
+        let issueDiv = document.createElement('div');
+        issueDiv.className = 'issue';
+        issueDiv.append(repo);
+        issueDiv.append(issueNumber);
+        issueDiv.append(content);
+
+        let githubIssuesDiv = document.getElementById('issues');
+        githubIssuesDiv.append(issueDiv);
+    })
+}
